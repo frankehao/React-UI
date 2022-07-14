0e0c0c805dddd15ff3019ba9af3910347d64e419
hao <2524129018@qq.com>
2022-07-14 12:07:45 +0800

    完成介绍页路由设置

--- example.tsx
+++ example.tsx
@@ -7,6 +7,7 @@ import IconDemo from './lib/icon/icon.demo';
 import ButtonDemo from './lib/button/button.demo';
 import DialogDemo from './lib/dialog/dialog.demo';
 import LayoutDemo from './lib/layout/layout.demo';
+import Introduce from './lib/doc/introduce';
 
 const logo = require('./logo.png');
 
@@ -21,7 +22,13 @@ ReactDOM.render(
       </Header>
       <Layout>
         <Aside className="site-aside">
-          <h2> <strong>组件</strong> </h2>
+          <h2><strong>组件</strong></h2>
+          <ul>
+            <li>
+              <NavLink to="/introduce">介绍</NavLink>
+            </li>
+          </ul>
+          <h2><strong>组件</strong></h2>
           <ul>
             <li>
               <NavLink to="/button">按钮</NavLink>
@@ -38,6 +45,7 @@ ReactDOM.render(
           </ul>
         </Aside>
         <Content className="site-main">
+          <Route path="/introduce" component={Introduce}/>
           <Route path="/icon" component={IconDemo}/>
           <Route path="/button" component={ButtonDemo}/>
           <Route path="/dialog" component={DialogDemo}/>
---
+++ lib/doc/introduce.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Introduce: React.FunctionComponent = () => {
+  return (
+    <div>x</div>
+  );
+};
+
+export default Introduce;
